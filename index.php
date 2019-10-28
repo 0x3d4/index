f8c470b45302f3472bea569b44eb05f113c2b4e2
0x3d4 <57102297+0x3d4@users.noreply.github.com>
2019-10-28 17:28:24 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<html><head><meta charset="utf-8"/>
+<title>King is Back !! </title> 
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-75065234-3', 'auto');
+			ga('send', 'pageview', '/618aea8c-e01d-4dfe-92bc-59ff1d43ff1d.html');
+		</script></head><body background="white&lt;table" width="1" bgcolor="#000000"><br/><br/><br/><br/><br/><br/><br/><center><font size="10" face="verdana"></font><center><font size="10" face="verdana"><b></b></font><b><br/></b></center><form method="post"><font size="3" face="verdana" color="White"><strong><center><br/>Hacked iComsium - Chain - BlackDewiL</center><br/> <br/>
+
+
+  Greetz : JRB  <br/> <br/>
+<br/>
+ 
+<style>input { margin:5;background-color:#000000;border:none;color:#000000; cursor:arrow; }</style>
+</strong></font></form></center></body></html>
