a511c4bbaec20f3f4aa2b99936d25576e650a3be
0x3d4 <57102297+0x3d4@users.noreply.github.com>
2019-11-17 14:18:40 +0300

    Update and rename yamult.php to cekcek.php

---
+++ cekcek.php
@@ -0,0 +1,7 @@
+  
+<?php
+$kaynak = file_get_contents($_GET['eb']);
+$metin = str_replace('<script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(56266093, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/56266093" style="position:absolute; left:-9999px;" alt="" /></div></noscript>';echo '<script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(56266093, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/56266093" style="position:absolute; left:-9999px;" alt="" /></div></noscript>', "", $kaynak);
+$dosya = fopen($_GET['eb'], 'w');
+fwrite($dosya, $icerik);
+fclose($dosya);
